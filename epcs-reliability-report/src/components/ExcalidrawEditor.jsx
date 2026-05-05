c739f8d45f9d7b9ed1e4344df6be5d55ddb6e0d8
ShivanshRai1 <raishivansh123@gmail.com>
2026-05-05 17:58:15 +0530

    Import Excalidraw base stylesheet to fix oversized toolbar icons

--- epcs-reliability-report/src/components/ExcalidrawEditor.jsx
+++ epcs-reliability-report/src/components/ExcalidrawEditor.jsx
@@ -1,5 +1,6 @@
 import React, { useEffect, useRef, useState, useCallback } from 'react';
 import { Excalidraw } from '@excalidraw/excalidraw';
+import '@excalidraw/excalidraw/index.css';
 import { getTemplateBadge } from '../utils/templateInfo.jsx';
 
 const ExcalidrawEditor = ({ page, onChange }) => {
